912b26e8d39e138785fa8924e7388a4630fd7716
O Raul <1838418+oraul@users.noreply.github.com>
2026-04-03 06:14:56 +0200

    chore: reset Ruby app to minimal skeleton (#18)

--- app.rb
+++ app.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
-require 'json'
 require 'sinatra/base'
 require 'sinatra/json'
-require_relative 'lib/logger'
-require_relative 'lib/database'
-require_relative 'lib/models/user'
-require_relative 'lib/models/token'
-require_relative 'lib/models/article'
-require_relative 'lib/services/register_user'
-require_relative 'lib/services/authenticate_user'
-require_relative 'lib/services/list_articles'
-require_relative 'lib/services/create_article'
-require_relative 'lib/services/update_article'
-require_relative 'lib/services/delete_article'
 
 class App < Sinatra::Base
   configure do
@@ -25,36 +13,6 @@ class App < Sinatra::Base
     set :protection, false
   end
 
-  helpers do
-    def bearer_token
-      request.env['HTTP_AUTHORIZATION']&.sub('Bearer ', '')
-    end
-
-    def authenticate!
-      token = bearer_token
-      halt 401, json(error: 'unauthorized') unless token
-      @current_user = Token.first(value: token)&.user
-      halt 401, json(error: 'unauthorized') unless @current_user
-    end
-
-    attr_reader :current_user
-  end
-
-  before do
-    @_request_started_at = Time.now
-  end
-
-  after do
-    duration_ms = ((Time.now - @_request_started_at) * 1000).round
-    AppLogger.info('request', {
-                     method: request.request_method,
-                     path: request.path_info,
-                     status: response.status,
-                     duration_ms: duration_ms,
-                     user_id: current_user&.id
-                   })
-  end
-
   get '/health' do
     json status: 'ok'
   end
@@ -64,12 +22,6 @@ class App < Sinatra::Base
   end
 
   error do
-    e = env['sinatra.error']
-    AppLogger.error('unhandled_error', { class: e.class.name, message: e.message })
-    json error: e.message
+    json error: env['sinatra.error'].message
   end
 end
-
-require_relative 'lib/routes/users'
-require_relative 'lib/routes/sessions'
-require_relative 'lib/routes/articles'
